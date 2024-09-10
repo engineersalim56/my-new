40704219272d167dbb06e252b4c9bc5e18f7f2d6
salim ullah <salimrahman526@gmail.com>
2024-09-10 09:04:39 +0500

    basic of python

---
+++ varibles.py
@@ -0,0 +1,21 @@
+#python  dynamic in veriavle  it has no specific keywords for the variable type like float , integer and string
+#integer in python 
+number =11
+print("The value assigned to the number is ",number)
+#same is the above 
+number2=int(22.2)
+print("the integer value of the number2 is ",number2)
+#floa value can be assigned  to a veriable in two such a ways
+#1
+number=11.0
+print("Float value of the number is ",number)
+#secound method 
+number=float(11)
+print("The float valueof the number is ",number)
+
+#string in python 
+string="salim ullah fresh gratiudted software engineer"
+print("The strng value  is ",string)
+
+
+#
